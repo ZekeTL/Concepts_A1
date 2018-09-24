8833e1719289adfa19a140e0352957332ac30ee1
Deja Jackson <dejatjackson@gmail.com>
2018-09-23 22:05:22 -0400

    Parser Modifications

--- src/Parser.java
+++ src/Parser.java
@@ -302,6 +302,8 @@ public class Parser
             op = Relative_op.lt_operator;
         else if (tok.getTokType() == tokentype.le_operator)
             op = Relative_op.le_operator;
+        else if (tok.getTokType() == tokentype.colon_tok)
+            op = Relative_op.colon_tok;
         else
             throw new ParserException ("relational operator expected at row " +
                     tok.getRowNumber()  + " and column " + tok.getColumnNumber());
